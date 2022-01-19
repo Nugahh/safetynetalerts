5920ba61e37a4d51f33c22dc1562beec3b0ae7a1
Nugahh <nugah.dev@gmail.com>
2022-01-20 00:15:38 +0100

    added package

---
+++ safetynetalerts/src/main/java/com/example/safetynetalerts/SafetynetalertsApplication.java
@@ -0,0 +1,13 @@
+package com.example.safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertsApplication.class, args);
+	}
+
+}
